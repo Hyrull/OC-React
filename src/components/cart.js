f5ebd50abcf4d060e464cbe372e27e7727158e73
Hyrul <xavier.leonard@outlook.com>
2024-04-22 18:39:17 +0200

    P2C2

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,8 +1,11 @@
+import '../styles/cart.css'
+
 function Cart() {
     const monsteraPrice = 8
     const ivyPrice = 10
     const flowerPrice = 15
-    return (<div>
+    return (
+    <div className="lmj-cart">
         <h2>Panier</h2>
         <ul>
         <li>Monstera : {monsteraPrice}€</li>
@@ -10,7 +13,7 @@ function Cart() {
          <li>Fleurs : {flowerPrice}€</li>
         </ul>
           Total : {monsteraPrice + ivyPrice + flowerPrice}€
-          </div>)
-    }
+    </div>
+    )}
 
 export default Cart
